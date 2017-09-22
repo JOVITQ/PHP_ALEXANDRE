dab4da0bd8e34f1d8df8c03221535e7cdfbca071
JOVITQ <jovitamiala@gmail.com>
2017-09-22 14:52:39 +0200

    donnees.php
    Exercices

---
+++ donnees.php
@@ -0,0 +1,18 @@
+<?php
+$login=$_GET["login"];
+$password=$_GET["password"];
+if (isset($login) && isset($password) && empty($login )!==true && empty($password)!==true){
+require "connection.php";
+$state =$dbh->prepare("INSERT INTO Formulaire(login, password) VALUES (:login,:password)");
+$state->bindParam(':login',$login);
+$state->bindParam(':password',$password);
+
+
+$state->execute();
+
+}
+else {
+echo "Your information is incomplete";
+}
+
+?>
