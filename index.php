8eb91a76d5681b7becf3d050acbf1fb472d3cd12
JOVITQ <jovitamiala@gmail.com>
2017-09-22 14:51:08 +0200

    index.php
    Exercices

---
+++ index.php
@@ -0,0 +1,13 @@
+
+
+<!DOCTYPE html>
+    <html>
+    <head>
+<meta charset="utf-8">
+    </head>
+<body>
+<form action="donnees.php" method="GET">
+<label for="login">Login</label> :<input type=text name=login "value">
+<label for="password">Password</label><input type=text name=password "value">
+<input type=submit name=send "value">
+</form>
